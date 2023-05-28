14d74b4cbf49e75d9ebc759141517a7a40d8b3a8
afcarrera <acarrera_l@hotmail.com>
2023-05-28 15:53:22 -0500

    Cambiar a proyecto con Azure Function

--- google-scholar-api/src/main/java/com/digitalnao/googlescholarapi/service/IGoogleScholarAuthorService.java
+++
@@ -1,7 +0,0 @@
-package com.digitalnao.googlescholarapi.service;
-
-import com.digitalnao.googlescholarapi.dto.SearchResultDTO;
-
-public interface IGoogleScholarAuthorService {
-    SearchResultDTO findById(String [] params);
-}
